ea7e71a7d01236fc3b10539f04c7f8a4426086b6
HybridDog <adresse_test@t-online.de>
2014-08-09 23:49:45 +0200

    automatic update
    Former-commit-id: ab1b9629a6862925407c285d38b867b3a0b3e8fb
    Former-commit-id: f91eb380c0feeadd55bd3caddef0ee736c0db84c

--- init.lua
+++ init.lua
@@ -77,12 +77,13 @@ if minetest.setting_getbool("creative_mode") then
 	minetest.register_on_punchnode(function(pos, node, puncher)
 		if puncher:get_wielded_item():get_name() == "creative:pick"
 		and node.name ~= "air" then
-			minetest.after(0.3, function(pos)
-				if minetest.get_node(pos).name ~= "air" then
+			minetest.after(0.3, function(pos, name)
+				if minetest.get_node(pos).name ~= "air"
+				and not minetest.is_protected(pos, name) then
 					print("[superpick] force destroying node at ("..pos.x.."|"..pos.y.."|"..pos.z..")")
 					minetest.remove_node(pos)
 				end
-			end, pos)
+			end, pos, puncher:get_player_name())
 			add_to_inv(puncher, node)
 		end
 	end)
